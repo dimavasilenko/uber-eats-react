9f1dbcadb32c85a6d4821686658dcca46e98d41f
dima <dima.vasilenko.96@gmail.com>
2019-07-01 20:39:36 +0300

    deleted header logo

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,9 +5,7 @@ import footer_logo from "./images/white-logo.svg";
 export function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__wrapper">
-        <img src={footer_logo} alt="footer" />
-      </div>
+      <div className="footer__wrapper"></div>
     </footer>
   );
 }
