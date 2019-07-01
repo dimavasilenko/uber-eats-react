4a9f61dc6bd757155728472a6e15213e3acbaef0
dima <dima.vasilenko.96@gmail.com>
2019-07-01 12:59:59 +0300

    added buttons folder

--- src/components/authorization-options/authorization-options.jsx
+++ src/components/authorization-options/authorization-options.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './authorization-options.css'
 import logo from './images/basket.svg';
+import '../buttons/buttons.css';
 
 export function AuthorizationOptions() {
     return (
         <div className="authorization-options">
-            <button className="authorization-options__sign-in">Sign In</button>
-            <button className="authorization-options__register">Register</button>
+            <button className="button">Sign In</button>
+            <button className="button button--register">Register</button>
             <a href="https://www.ubereats.com/ru-UA/checkout/">
                 <img src={logo} alt="basket-logo"/>
             </a>
