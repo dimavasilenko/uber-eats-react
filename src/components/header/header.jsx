d8624ddb8f4e88d515c0c6f4e726ed0fa63647f8
dima <dima.vasilenko.96@gmail.com>
2019-07-01 20:37:57 +0300

    deleted header logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,12 +7,6 @@ export function Header() {
   return (
     <header className="header">
       <div className="header__wrapper">
-        <div className="header__logo-wrapper">
-          <a
-            className="header__logo"
-            href="https://www.ubereats.com/uk-UA/"
-          ></a>
-        </div>
         <DeliveryTerms />
         <AuthorizationOptions />
       </div>
