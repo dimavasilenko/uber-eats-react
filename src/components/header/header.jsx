c4f8bbef858fea2be1f124666fb88f662989531c
dima <dima.vasilenko.96@gmail.com>
2019-07-02 11:31:27 +0300

    added header_logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./header.css";
 import { DeliveryTerms } from "../delivery-terms/delivery-terms";
 import { AuthorizationOptions } from "../authorization-options/authorization-options";
-
+import header_logo from "./images/header-logo.svg";
 export function Header() {
   return (
     <header className="header">
       <div className="header__wrapper">
+        <img src={header_logo} alt="header" />
         <DeliveryTerms />
         <AuthorizationOptions />
       </div>
