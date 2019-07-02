c5fe73daa514a7ce224253d55b91220420384316
dima <dima.vasilenko.96@gmail.com>
2019-07-02 11:24:33 +0300

    Revert "deleted header logo"
    This reverts commit 9f1dbcad

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,7 +5,9 @@ import footer_logo from "./images/white-logo.svg";
 export function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__wrapper"></div>
+      <div className="footer__wrapper">
+        <img src={footer_logo} alt="footer" />
+      </div>
     </footer>
   );
 }
