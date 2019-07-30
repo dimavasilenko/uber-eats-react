94cb22d799db507f03c0e357815a963aeda0eb79
dima <dima.vasilenko.96@gmail.com>
2019-07-30 15:15:26 +0300

    try to do render data from server

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -10,14 +10,31 @@ export class MainPage extends React.Component {
     super(props);
 
     this.state = {
-      searchValue: ""
+      searchValue: "",
+      error: null,
+      isLoaded: false,
+      restaurants: []
     };
   }
 
   updateSearchValue = searchValue => {
-    this.setState({
-      searchValue: searchValue
-    });
+    fetch("https://uber-eats-mates.herokuapp.com/api/v1/restaurants")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            searchValue: searchValue,
+            isLoaded: true,
+            restaurants: result.restaurants
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
   };
 
   searchInCategories(restaurant) {
@@ -35,37 +52,44 @@ export class MainPage extends React.Component {
 
   render() {
     const city = "Kyiv Restaurant";
-    return (
-      <div className="main-page">
-        <div className="main-page_wrapper">
-          <Search onSearchChange={this.updateSearchValue} />
-          <span className="main-page__city">{city}</span>
-          <ul className="main-page__list">
-            <div className="main-page__grid">
-              {restaurant
-                .filter(
-                  restaurant =>
-                    restaurant.title
-                      .toLowerCase()
-                      .includes(this.state.searchValue) ||
-                    this.searchInCategories(restaurant)
-                )
-                .map((restaurant, i) => {
-                  return (
-                    <RestaurantCard
-                      key={i}
-                      title={restaurant.title}
-                      categories={restaurant.categories}
-                      priceBucket={restaurant.priceBucket}
-                      etaRange={restaurant.etaRange}
-                      imageUrl={restaurant.imageUrl}
-                    />
-                  );
-                })}
-            </div>
-          </ul>
+    const { error, isLoaded, restaurants } = this.state;
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <div className="main-page">
+          <div className="main-page_wrapper">
+            <Search onSearchChange={this.updateSearchValue} />
+            <span className="main-page__city">{city}</span>
+            <ul className="main-page__list">
+              <div className="main-page__grid">
+                {restaurant
+                  .filter(
+                    restaurant =>
+                      restaurant.title
+                        .toLowerCase()
+                        .includes(this.state.searchValue) ||
+                      this.searchInCategories(restaurant)
+                  )
+                  .map((restaurant, i) => {
+                    return (
+                      <RestaurantCard
+                        key={i}
+                        title={restaurant.title}
+                        categories={restaurant.categories}
+                        priceBucket={restaurant.priceBucket}
+                        etaRange={restaurant.etaRange}
+                        imageUrl={restaurant.imageUrl}
+                      />
+                    );
+                  })}
+              </div>
+            </ul>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
